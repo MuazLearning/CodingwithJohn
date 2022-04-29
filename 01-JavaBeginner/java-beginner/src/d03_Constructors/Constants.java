36a1478ce785e2ebcb8cb223e5c633bd774fba74
Muaz MEMİŞ <muazmemis@gmail.com>
2022-04-29 15:05:23 +0300

    Lesson 3 - Java Constructors - Full Tutorial

---
+++ 01-JavaBeginner/java-beginner/src/d03_Constructors/Constants.java
@@ -0,0 +1,16 @@
+package d03_Constructors;
+
+public class Constants {
+
+    public Constants() {
+
+    }
+
+//    private Constants() {
+//
+//    }
+
+    public static final int NUMBER_OF_MONTHS_IN_A_YEAR = 12;
+    public static final int NUMBER_OF_MONTHS_IN_A_WEEK = 7;
+    public static final String MY_NAME = "MUAZ";
+}
---
+++ 01-JavaBeginner/java-beginner/src/d03_Constructors/Constructors.java
@@ -0,0 +1,16 @@
+package d03_Constructors;
+
+public class Constructors {
+    public static void main(String[] args) {
+//        Dog myDog = new Dog();
+//        Dog myDog = new Dog("Boncuk");
+//        Dog myDog = new Dog(2);
+//        Dog myDog = new Dog("Tom", 2);
+
+//        System.out.println(myDog.name + " : " + myDog.age);
+
+        Constants myConstants = new Constants();
+        myConstants.
+        System.out.println(Constants.MY_NAME);
+    }
+}
