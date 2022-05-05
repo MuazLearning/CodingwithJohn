92dab443035c4a88bde8f10548c607c398a805e4
Muaz MEMİŞ <muazmemis@gmail.com>
2022-05-05 11:27:05 +0300

    Lesson 12 - Java Main Method Explained - What Does All That Stuff Mean?

---
+++ 01-JavaBeginner/java-beginner/src/d12_MainMethodExplanation/MainMethodExplanation.java
@@ -0,0 +1,13 @@
+package d12_MainMethodExplanation;
+
+public class MainMethodExplanation {
+    public static void main(String[] args) {
+
+//        MainMethodExplanation.main(args);
+//        MainMethodExplanation explanation = new MainMethodExplanation();
+//        explanation.main(args);
+
+        System.out.println(args[0]);
+
+    }
+}
