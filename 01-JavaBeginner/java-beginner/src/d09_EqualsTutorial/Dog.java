46c3e3d19adcd2b00db960dba8e27b81a9c6bb39
Muaz MEMİŞ <muazmemis@gmail.com>
2022-05-05 08:59:08 +0300

    Lesson 9 - .equals() vs. == in Java - The Real Difference

---
+++ 01-JavaBeginner/java-beginner/src/d09_EqualsTutorial/Dog.java
@@ -0,0 +1,27 @@
+package d09_EqualsTutorial;
+
+public class Dog {
+
+    private int age;
+    private String name;
+
+//    public boolean equals(Object obj){
+//
+//    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
---
+++ 01-JavaBeginner/java-beginner/src/d09_EqualsTutorial/EqualsTutorial.java
@@ -0,0 +1,46 @@
+package d09_EqualsTutorial;
+
+public class EqualsTutorial {
+    public static void main(String[] args) {
+/*        int int1 = 4;
+        int int2 = 4;
+
+        if (int1 == int2)
+            System.out.println("The numbers are equal");
+        else
+            System.out.println("The numbers are not equal");*/
+
+/*        String string1 = new String("hello");
+        String string2 = new String("hello");
+
+        if (string1 == string2)
+            System.out.println("The strings are equal");
+        else
+            System.out.println("The strings are not equal");*/
+
+/*        String string1 = new String("hello");
+        String string2 = new String("hello");
+
+        if (string1.equals(string2))
+            System.out.println("The strings are equal");
+        else
+            System.out.println("The strings are not equal");*/
+
+        // True
+        String string1 = "hello";
+        String string2 = "hello";
+
+        if (string1 == string2)
+            System.out.println("The strings are equal");
+        else
+            System.out.println("The strings are not equal");
+
+/*        Dog myDog = new Dog();
+        Dog yourDog = new Dog();
+
+        if (myDog.equals(yourDog))
+            System.out.println("The dogs are equal");
+        else
+            System.out.println("The dogs are not equal");*/
+    }
+}
